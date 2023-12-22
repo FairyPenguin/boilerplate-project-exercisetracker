1870371cb3950861b6f1e6ea643e2c996878354c
FairyPenguin <autorun81@gmail.com>
2023-12-22 11:11:05 +0200

    Respond with username and random-generated id to the POST /api/users end point

--- index.js
+++ index.js
@@ -1,18 +1,78 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const crypto = require("crypto");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+const users = [];
 
+users.push({ username: "fcc_test", _id: "5fb5853f734231456ccb3b05" });
 
+let log = [];
 
+let exercisesCount = 0;
+
+users.find((user) => {
+  (user === user._id) === "5fb5853f734231456ccb3b05";
+
+  const objectToPush = {
+    description: "test",
+    duration: 60,
+    date: "Mon Jan 01 1990",
+  };
+
+  if (!objectToPush.date || objectToPush.date === "") {
+    objectToPush.date = new Date().toDateString();
+  }
+  log.push(objectToPush);
+
+  const count = exercisesCount;
+
+  exercisesCount++;
+
+  user.count = count;
+
+  return (user.log = log);
+});
+
+console.log(users[0]);
+
+// const a = { description: "a object", duration: 60, date: "" };
+
+// if (!a.date || a.date === "") {
+//   a.date = new Date().toDateString();
+//   console.log(a);
+// }
+
+const usersArray = [];
+
+app.post("/api/users", (req, res) => {
+  // username
+  const userName = req.body.username;
+  //user id - randomly generated
+  const userId = crypto.randomBytes(12).toString("hex");
+
+  res.json({ username: userName, _id: userId });
+});
+
+app.post("/api/users:_id/exercises", (req, res) => {
+  const userName = req.body.username;
+  console.log(userName);
+  res.json({ username: userName });
+});
+
+app.get("/", (req, res) => {
+  res.json("-------|------");
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
