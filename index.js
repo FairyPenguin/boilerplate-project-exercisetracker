97deca7ee2936d0058c780b08585eb6c2f93c507
FairyPenguin <autorun81@gmail.com>
2023-12-22 13:10:39 +0200

    Respond with the usee object contains excercise log to POST /api/users/:_id/exercises

--- index.js
+++ index.js
@@ -52,9 +52,14 @@ users.find((user) => {
 //   console.log(a);
 // }
 
+// Users Arrays
 const usersArray = [];
 
-// ::POST => /api/users end-point
+// ::
+//  ==>--------First route end-points----------<==
+// ::
+
+// 1- ::POST => /api/users end-point
 
 app.post("/api/users", (req, res) => {
   // username
@@ -67,13 +72,59 @@ app.post("/api/users", (req, res) => {
   res.json({ username: userName, _id: userId });
 });
 
-// ::GET => /api/users end-point
+// 2- ::GET => /api/users end-point
 
 app.get("/api/users", (req, res) => {
   res.json(usersArray);
 });
 
-app.post("/api/users:_id/exercises", (req, res) => {
+// ::
+//  ==>--------Second route end-points----------<==
+// ::
+
+// 1- ::POST => /api/users end-point
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  //excersices log array
+  const log = [];
+  let matchedUser;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  const date = req.body.date;
+  const userIdParam = req.params._id;
+
+  if (!date || date === "") {
+    new Date().toDateString();
+  }
+
+  /* 
+  match the user id from the excercise data to the user
+  id in the users array
+    */
+  usersArray.find((user) => {
+    user._id === userIdParam;
+
+    // pusth the exercise data to the log array
+
+    log.push({
+      description: String(description),
+      duration: parseInt(duration),
+      date: date,
+    });
+
+    // add the log array to the user object in the users array
+
+    user.log = log;
+    console.log(user);
+    matchedUser = user;
+  });
+
+  res.json(matchedUser);
+});
+
+// 2- ::GET => /api/users end-point
+
+app.get("/api/users:_id/exercises", (req, res) => {
   const userName = req.body.username;
   console.log(userName);
   res.json({ username: userName });
